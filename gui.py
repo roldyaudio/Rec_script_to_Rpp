5dd2c83c65f1504bc8745ff798e62935631a2e71
rrobles <roldyaudio@gmail.com>
2025-07-03 13:10:39 +0200

    install requirements added

--- gui.py
+++ gui.py
@@ -1,1 +1,21 @@
+import subprocess
+import sys
+
+def install_requirements():
+    try:
+        result = subprocess.run([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"],
+                                capture_output=True, text=True)
+
+        if result.returncode == 0:
+            print("Dependencies installed successfully.")
+            if "Requirement already satisfied" in result.stdout:
+                print("Some packages were already installed.")
+        else:
+            print(f"Error installing dependencies: {result.stderr}")
+            sys.exit(1)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        sys.exit(1)
+
 print("Working fine")
+install_requirements()
