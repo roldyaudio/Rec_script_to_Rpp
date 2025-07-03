2823ed72e6311699890d8ad7c043e86ae4a47e29
rrobles <roldyaudio@gmail.com>
2025-07-03 13:22:26 +0200

    First gui added

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
+import customtkinter as ctk
 import subprocess
 import sys
 
+# Package installation
 def install_requirements():
     try:
         result = subprocess.run([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"],
@@ -17,5 +19,31 @@ def install_requirements():
         print(f"An unexpected error occurred: {e}")
         sys.exit(1)
 
-print("Working fine")
+
+def center_hub(window, width: int, height: int):
+    """Centers the window to the main display/monitor"""
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = int((screen_width / 2) - (width / 2))
+    y = int((screen_height / 2) - (height / 2))
+    window.geometry(f"{width}x{height}+{x}+{y}")
+
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        # Configure
+        self.title("App hub Launcher")
+        self.grid_rowconfigure(0, weight=1)  # configure grid system
+        self.grid_columnconfigure(0, weight=1)
+
+
+
+
+
+
+
 install_requirements()
+app = App()
+center_hub(app, 280, 320)
+app.mainloop()
